c4c854519c78d5cb0ddeb0f5a269cf8d1ccb08e4
Florian Bergmann <Bergmann.F@gmail.com>
2013-09-13 00:11:31 +0200

    Changed the require to using. Pulled out the frame into its own def.

--- src/csvparser/swingui.clj
+++ src/csvparser/swingui.clj
@@ -1,10 +1,10 @@
 (ns csvparser.swingui
   (:gen-class)
-  (:use seesaw.core)
-  (:use seesaw.chooser)
-  (:use csvparser.core)
-  (:use seesaw.mig)
-  (:require [seesaw.bind :as b])
+  (:use [csvparser.core]
+        [seesaw.core]
+        [seesaw.chooser]
+        [seesaw.mig]
+        [seesaw.bind :only (bind)])
   (:import org.pushingpixels.substance.api.SubstanceLookAndFeel
            javax.swing.UIManager
            java.io.File
@@ -36,10 +36,10 @@
   (let [data (process-file @chosen-source-file simple-process)]
     (write-data-to-file @chosen-target-file data)))
 
-(def source-button (button :text "Quelle Waehlen"
+(def source-button (button :text "Quelle Wählen"
                            :listen [:action choose-source-file]))
 
-(def target-button (button :text "Ziel Waehlen"))
+(def target-button (button :text "Ziel Wählen"))
 
 (def cancel-button (button :text "Abbrechen"
                            :listen [:action (fn [x] (System/exit 0))]))
@@ -48,6 +48,10 @@
                        :listen [:action
                                 (fn [x] (process-data @chosen-source-file @chosen-target-file))]))
 
+(def data-table (table
+                 :model [:columns [{:key :id :text "Id"}]
+                         :rows [{:id "Test"}]]))
+
 (def all-encodings (java.nio.charset.Charset/availableCharsets))
 
 (def utf-8-enc (first (filter #(.contains (.toString %1) "UTF-8") all-encodings)))
@@ -55,20 +59,23 @@
 (def encodings-combobox
   (selection! (combobox :model (vec all-encodings)) utf-8-enc))
 
+(def main-window (frame :title "CSV-Parser"
+                        :content "",
+                        :on-close :exit
+                        :menubar (menubar :items
+                                          [(menu :text "File" :items [])
+                                           (menu :text "Help" :items [])])
+                        :content (mig-panel
+                                  :constraints ["wrap 3" "[grow][][]" ""]
+                                  :items [[source-txt "growx"] [source-button "grow"] [encodings-combobox ""]
+                                          [target-txt "growx"] [target-button "grow, wrap"]
+                                          [cancel-button ""] [ok-button "wrap"]
+                                          [data-table "grow"]])))
+
 (defn -main [& args]
   (SubstanceLookAndFeel/setSkin
    "org.pushingpixels.substance.api.skin.BusinessBlueSteelSkin")
   (invoke-later
-   (-> (frame :title "CSV-Parser"
-              :content "",
-              :on-close :exit
-              :menubar (menubar :items
-                                [(menu :text "File" :items [])
-                                 (menu :text "Help" :items [])])
-              :content (mig-panel
-                        :constraints ["wrap 3" "[grow][][]" ""]
-                        :items [[source-txt "growx"] [source-button "grow"] [encodings-combobox ""]
-                                [target-txt "growx"] [target-button "grow, wrap"]
-                                [cancel-button ""] [ok-button ""]]))
+   (-> main-window
        pack!
        show!)))
